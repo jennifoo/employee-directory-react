aa78ecdcf1d004028dbd62434a275aca11dfcb66
Jennifer Wong <jennifer.design@yahoo.com>
2020-10-02 18:38:20 -0500

    Set up Form state

---
+++ src/components/Form/index.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+import ".style.css";
+import Title from "../Title"
+
+class Form extends Component {
+  state = {
+    search: ""
+  };
+
+
+render() {
+
+  <Title />
+
+}
+}
+
+export default Form;

83c4f200d234f76221374a0f13fd95b7956df74c
Jennifer Wong <jennifer.design@yahoo.com>
2020-10-02 18:48:37 -0500

    Set up Title component shell and content

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -1,23 +1,23 @@
 import React, { Component } from "react";
-import ".style.css";
+import "./style.css";
 import Title from "../Title"
 
 class Form extends Component {
   state = {
     search: ""
   };
-
-  handleInputChange = event => {
-    let value = event.target.value;
-
-    this.setState({
-      [name]: value
-    });
-  };
+  //
+  // handleInputChange = event => {
+  //   let value = event.target.value;
+  //
+  //   this.setState({
+  //     [name]: value
+  //   });
+  // };
 
 render() {
 
-  <Title />
+  return <Title />
 
 }
 }

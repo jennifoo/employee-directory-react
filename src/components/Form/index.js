059060bdff5a91f50c1a5110fc713763b409d2a4
Jennifer Wong <jennifer.design@yahoo.com>
2020-10-02 23:53:53 -0500

    Created API GET result using axios to obtain list of random employees

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -1,11 +1,21 @@
 import React, { Component } from "react";
+import API from "../../utils/API";
 import "./style.css";
-import Title from "../Title"
+import Title from "../Title";
 
 class Form extends Component {
   state = {
-    search: ""
+    search: "",
+    results: []
   };
+
+  // When the component mounts, get a list employees and update this.state.results
+  componentDidMount() {
+    API.getEmployeeList()
+      .then(res => this.setState({ results: res.data })) // Array of 200 employees
+      .catch(err => console.log(err));
+      // .then(res => console.log(res.data.results))
+  }
 
   handleInputChange = event => {
     let search = event.target.name
@@ -29,7 +39,6 @@ render() {
     />
     </>
   )
-
 
 }
 }

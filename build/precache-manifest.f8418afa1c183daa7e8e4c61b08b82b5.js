748a27ef00a835dabc9976efc7d0216a1f2477bb
Jennifer Wong <jennifer.design@yahoo.com>
2020-10-04 01:19:22 -0500

    Deploy setting adjustment

---
+++ build/precache-manifest.f8418afa1c183daa7e8e4c61b08b82b5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1b97980d6a620cefb81d5d9ef5ffa5f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "36aca9bd357b8982e548",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "5729b475fb4ec88ef2cc",
+    "url": "/static/css/main.89d23b8e.chunk.css"
+  },
+  {
+    "revision": "36aca9bd357b8982e548",
+    "url": "/static/js/2.3f0230c4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.3f0230c4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5729b475fb4ec88ef2cc",
+    "url": "/static/js/main.7b653686.chunk.js"
+  },
+  {
+    "revision": "0239a00348e1c49388c7",
+    "url": "/static/js/runtime-main.898530a7.js"
+  }
+]);
---
+++ server.js
@@ -0,0 +1,20 @@
+var express = require("express");
+var path = require("path");
+var fs = require("fs");
+
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});
